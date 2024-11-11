b4a93faa9919ea6cbb58462ca90768badf330c58
Darius Fang <dariusfang1@gmail.com>
2024-11-10 22:36:11 -0700

    character movement
    start of movement

---
+++ Assets/Scripts/Character/InputController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputController : MonoBehaviour
+{
+    private Vector3 moveDirection;
+    [SerializeField] MovementModule movementModule;
+    [SerializeField] FishingModule fishingModule;
+    [SerializeField] InteractModule interactModule;
+    public bool isMoving { get; private set; }
+    private void Update() {
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.z = Input.GetAxisRaw("Vertical");
+
+        if(movementModule != null)
+        {
+            isMoving = moveDirection != Vector3.zero;
+            if(isMoving)
+            {
+                movementModule.MoveCharacter(moveDirection);
+            }
+        }
+        if(interactModule != null && Input.GetKeyDown(KeyCode.F))
+        {
+            isMoving = moveDirection != Vector3.zero;
+            Debug.Log("Interacting");
+        }
+        if(fishingModule != null && Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("fishing");
+        }
+
+
+    }
+}
