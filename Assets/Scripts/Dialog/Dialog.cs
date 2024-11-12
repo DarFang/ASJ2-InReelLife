d241c20677626f98cabea485bb950f64f3b2f58b
Darius Fang <dariusfang1@gmail.com>
2024-11-12 16:48:23 -0700

    added richtext

--- Assets/Scripts/Dialog/Dialog.cs
+++ Assets/Scripts/Dialog/Dialog.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Text.RegularExpressions;
 public class Dialog : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textComponent;
@@ -36,10 +37,35 @@ public class Dialog : MonoBehaviour
     }
     IEnumerator TypeLine()
     {
-        foreach (char c in lines[index].ToCharArray())
+        string richTextPattern = @"<.*>";
+        if (Regex.IsMatch(lines[index], richTextPattern))
         {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
+            MatchCollection matches = Regex.Matches(lines[index], richTextPattern);
+            int textIndex = 0;
+            int tagIndex = 0;
+            while (textIndex < lines[index].Length)
+            {
+                if(tagIndex < matches.Count && matches[tagIndex].Index == textIndex)
+                {
+                    textComponent.text += matches[tagIndex].Value;
+                    textIndex += matches[tagIndex].Length;
+                    tagIndex ++;
+                }
+                else
+                {
+                    textComponent.text += lines[index][textIndex];
+                    textIndex ++;
+                    yield return new WaitForSeconds(textSpeed);
+                }
+            }
+        }
+        else
+        {
+            foreach (char c in lines[index])
+            {
+                textComponent.text += c;
+                yield return new WaitForSeconds(textSpeed);
+            }
         }
     }
     void NextLine()
