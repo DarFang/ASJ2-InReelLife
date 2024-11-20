155d4603ba445ad17bc3ec8faaa5e72f39b77343
Darius Fang <dariusfang1@gmail.com>
2024-11-19 21:32:06 -0700

    updated shop with selection

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    List<ItemDisplay> ShopItemDisplay;
-    [SerializeField] ItemDisplay itemDisplay;
-    List<ShopItem> shopItems;
+    List<ShopDisplay> ShopItemDisplay;
+    [SerializeField] ShopDisplay shopDisplay;
+    [SerializeField] Transform SellTransform;
+    [SerializeField] List<ShopItem> shopItems;
+    int SelectedIndex = 0;
     private void Start() 
     {
-        ShopItemDisplay = new List<ItemDisplay>();
+        ShopItemDisplay = new List<ShopDisplay>();
         InitializeInventory();
+    }
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (SelectedIndex - 1 >= 0)
+            {
+                ShopItemDisplay[SelectedIndex].UnSelect();
+                SelectedIndex -= 1;
+                ShopItemDisplay[SelectedIndex].Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (SelectedIndex + 1 < ShopItemDisplay.Count)
+            {
+                ShopItemDisplay[SelectedIndex].UnSelect();
+                SelectedIndex += 1;
+                ShopItemDisplay[SelectedIndex].Select();
+            }
+        }
     }
 
     void InitializeInventory()
     {
+        SelectedIndex = 0;
         foreach(ShopItem shopItem in shopItems)
         {
-            ItemDisplay temp = Instantiate(itemDisplay, transform);
+            ShopDisplay temp = Instantiate(shopDisplay, SellTransform);
+            temp.Initialize(shopItem);
             ShopItemDisplay.Add(temp);
         }
+        ShopItemDisplay[SelectedIndex].Select();
     }
+
 
 }
--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "ShopItem", menuName = "ShopItem_", order = 3)]
 public class ShopItem : ScriptableObject
 {
     public int Cost {get{return cost;}}
     [SerializeField] int cost = 1;
+    [SerializeField] public Sprite Sprite;
+    [SerializeField] public String Name;
     public void PurchaseItem()
     {
         GameManager.Instance.PurchaseItem(this);
