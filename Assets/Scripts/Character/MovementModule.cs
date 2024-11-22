7dc536481977715245a1a45c78c389844036a955
Darius Fang <dariusfang1@gmail.com>
2024-11-22 00:57:46 -0700

    updated gravity for player

--- Assets/Scripts/Character/MovementModule.cs
+++ Assets/Scripts/Character/MovementModule.cs
@@ -11,6 +11,13 @@ public class MovementModule : MonoBehaviour
     [SerializeField] private float movementSpeed;
     private Vector3 moveDirection;
 
+    [Header("Gravity")]
+    public float gravityStrength = 9.81f;
+    public float terminalVelocity = -50f; 
+    private void Update() 
+    {
+         ApplyGravity();   
+    }
     public void MoveCharacter(Vector3 direction)
     {
         moveDirection.x = direction.x;
@@ -19,14 +26,32 @@ public class MovementModule : MonoBehaviour
         float tempMultiplier = 1;
 
         controller.Move(((transform.right * moveDirection.x) + (transform.forward * moveDirection.z)) * Time.deltaTime * movementSpeed * tempMultiplier);
-        RotateCharacter(direction);
-        
-        
+        RotateCharacter(direction);     
     }
 
     public void RotateCharacter(Vector3 direction)
     {
         float yAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         rotationTransform.rotation = Quaternion.Euler(0, yAngle , 0);
+    }
+
+
+
+    private void ApplyGravity()
+    {
+        Vector3 velocity = new Vector3();
+        if (controller.isGrounded)
+        {
+            velocity.y = -0.5f;
+        }
+        else
+        {
+            velocity.y -= gravityStrength * Time.deltaTime;
+            if (velocity.y < terminalVelocity)
+            {
+                velocity.y = terminalVelocity;
+            }
+        }
+        controller.Move(velocity);
     }
 }
